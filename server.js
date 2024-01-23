84ff174d3ebcfe55e3c3b6ea7de8025196923bc7
begumayten <79323462+begumayten@users.noreply.github.com>
2024-01-23 21:41:10 +0300

    ui fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/images', express.static('images'));
 const dbConfig = {
     host: '127.0.0.1',
     user: 'root',
-    password: '30112619Gulsah',
+    password: 'emre12',
     database: 'ticket',
 };
 
